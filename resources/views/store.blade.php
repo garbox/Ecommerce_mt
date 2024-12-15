423fce9be060e496ef68067f4c30fe0fca636011
Kyle Metivier <metivier.kyle@yahoo.com>
2024-12-14 20:40:05 -0600

    Update store.blade.php

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -28,6 +28,10 @@
             @endforeach
         </div>
     </div>
-      <!-- Footer -->
 <!-- Footer -->
 <x-footer />
+  <!-- Bootstrap JS & Popper.js -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
