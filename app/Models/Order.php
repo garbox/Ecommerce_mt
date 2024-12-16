8547a54667a5b9dc19418056d01da7cddf1c557e
Kyle Metivier <metivier.kyle@yahoo.com>
2024-12-16 11:15:29 -0600

    Update Order.php
    added orderitem model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,7 @@ use App\Models\ProductAttribute;
 use App\Models\CartAttribute;
 use App\Models\Cart;
 use App\Models\Status;
+use App\Models\Orderitem;
 use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
