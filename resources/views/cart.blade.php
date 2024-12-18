9e291dbdd73abcb8978c42703b00a66c32891212
Kyle Metivier <metivier.kyle@yahoo.com>
2024-12-18 15:35:31 -0600

    Update cart.blade.php
    title change

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Cart with Bootstrap</title>
+    <title>Modern Tables - Cart</title>
     <!-- Link to Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
