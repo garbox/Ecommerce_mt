f4ce4a8eeb7755bdc635ed52d55928443d9b8748
Kyle Metivier <metivier.kyle@yahoo.com>
2024-12-19 16:58:03 -0600

    Update StatusController.php

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class StatusController extends Controller
 {
-    //
+    public function update(Request $request){
+        $request->validate([
+            'status' => 'int',
+        ]);
+
+        $order = Order::find($request->orderid);
+        $order->status_id = $request->status;
+
+        if ($order->save()){
+            return back()->with('success', 'Status updated!');
+        }
+        else{
+            return back()->with('error', 'Not able to update status!');
+        }
+
+       
+    }
 }
