96938ad9af1e35c4b1264f76b7ca8bd6f1882d87
Kyle Metivier <metivier.kyle@yahoo.com>
2024-12-23 12:47:18 -0600

    Update orderDetails.blade.php

--- resources/views/dashboard/orderDetails.blade.php
+++ resources/views/dashboard/orderDetails.blade.php
@@ -8,7 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body class="d-flex flex-column min-vh-100">
-<x-navigation/>
+  <x-dashboardnav/>
 
   <div class="container my-5">
     <!-- Page Header -->
