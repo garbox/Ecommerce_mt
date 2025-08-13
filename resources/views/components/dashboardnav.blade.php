06d7edd436bae85cefeeeceabb38fb5a04d223cf
Kyle Metivier <metivier.kyle@yahoo.com>
2025-08-12 19:28:53 -0500

    Navigation Clean up (drop down)

--- resources/views/components/dashboardnav.blade.php
+++ resources/views/components/dashboardnav.blade.php
@@ -1,34 +1,34 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/dashboard">Garbox Creations - {{ucwords(Route::currentRouteName())}}</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/dashboard">Dashboard</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/dashboard/products">Products</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/dashboard/createproduct">Create Product</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/dashboard/createtype">Create Type</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/dashboard/createattribute">Create Attributes</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/user/logout">Log Out</a>
-          </li>
-        </ul>
-      </div>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/dashboard">Garbox Creations - {{ucwords(Route::currentRouteName())}}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard">Dashboard</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Products
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
+            <li><a class="dropdown-item" href="/dashboard/products">All products</a></li>
+            <li><a class="dropdown-item" href="/dashboard/createproduct">Create Product</a></li>
+            <li><a class="dropdown-item" href="/dashboard/createtype">Create Type</a></li>
+            <li><a class="dropdown-item" href="/dashboard/createattribute">Create Attributes</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/user/logout">Log Out</a>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,34 +1,41 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Garbox Creations</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/store">Shop</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/cart">Cart</a>
-          </li>
-          @if (session()->get('id') != null)
-          <li class="nav-item">
-            <a class="nav-link" href="/orderstatus">Order Status</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/user/logout">Log Out</a>
-          </li>
-          @else
-          <li class="nav-item">
-            <a class="nav-link" href="/login">Login</a>
-          </li>
-          @endif
-        </ul>
-      </div>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Garbox Creations</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/store">Shop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/cart">Cart</a>
+        </li>
+        @if (session()->get('id') != null)
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profile
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
+            <li><a class="dropdown-item" href="/orderstatus">Order Status</a></li>
+            <li><a class="dropdown-item" href="/">Settings</a></li>
+            <li><a class="dropdown-item" href="/">Overview</a></li>
+            <li><a class="dropdown-item" href="/">Something else</a></li>
+            <hr>
+            <li><a class="dropdown-item" href="/user/logout">Log Out</a></li>
+          </ul>
+        </li>
+        @else
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+        @endif
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
